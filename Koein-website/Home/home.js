92be033952476c21fa61a68e46d37853f3d0d364
Andy-Merhi <62130103@students.liu.edu.lb>
2024-12-06 10:19:47 +0200

    applying the swipper slide to the homepage

---
+++ Koein-website/Home/home.js
@@ -0,0 +1,13 @@
+ // Initialize Swiper
+ const swiper = new Swiper('.swiper', {
+    // Configuration options
+    loop: true, // Loop through slides
+    autoplay: {
+      delay: 5000, // Auto-slide every 5 seconds
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true, // Allow pagination bullets to be clickable
+    }
+  });
