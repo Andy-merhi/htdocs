88e1d61751e76839bffb3386f7a16eff95f97c4b
Andy-Merhi <62130103@students.liu.edu.lb>
2024-12-16 17:49:53 +0200

    menu-1-2-3 done

--- Koein-website/Home/home.js
+++ Koein-website/Home/home.js
@@ -3,11 +3,43 @@
     // Configuration options
     loop: true, // Loop through slides
     autoplay: {
-      delay: 500000, // Auto-slide every 5 seconds
+      delay: 5000, // Auto-slide every 5 seconds
       disableOnInteraction: false,
     },
     pagination: {
       el: '.swiper-pagination',
       clickable: true, // Allow pagination bullets to be clickable
     }
+  });
+
+  // Function to handle menu-burger click
+  document.getElementById('menu-burger-icon').addEventListener('click', function() {
+    // Display something else
+    document.getElementById('menu-burger-items').style.display = 'block';
+  });
+
+  document.getElementById('menu-burger-exit').addEventListener('click', function() {
+    // Display something else
+    document.getElementById('menu-burger-items').style.display = 'none';
+  });
+
+
+   // Function to handle menu-burger click
+   document.getElementById('menu-burger-women').addEventListener('click', function() {
+    // Display something else
+    document.getElementById('menu-burger-items').style.display = 'none';
+    document.getElementById('menu-1').style.display = 'block';
+  });
+
+  document.getElementById('menu-1-back-arrow').addEventListener('click', function() {
+    // Display something else
+    document.getElementById('menu-1').style.display = 'none';
+    document.getElementById('menu-burger-items').style.display = 'block';
+  });
+
+   // Function to handle menu-burger click
+   document.getElementById('menu-1-clothing').addEventListener('click', function() {
+    // Display something else
+    document.getElementById('menu-1').style.display = 'none';
+    document.getElementById('menu-1-overlay').style.display = 'block';
   });
