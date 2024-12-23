60a89c749824815373649b68167f7c1cfb9e9a72
Andy-Merhi <62130103@students.liu.edu.lb>
2024-12-23 12:46:06 +0200

    sign in and sign up section done on homepage

--- Koein-website/Home/home.js
+++ Koein-website/Home/home.js
@@ -56,3 +56,32 @@
      
       document.getElementById('profile-container').style.display = 'block';
     });
+
+    function changeQuantity(amount) {
+      const quantityElement = document.getElementById("cartQuantity");
+      let currentQuantity = parseInt(quantityElement.innerText);
+    
+      // Adjust quantity but prevent it from going below 1
+      currentQuantity += amount;
+      if (currentQuantity < 1) {
+        currentQuantity = 1;
+      }
+    
+      quantityElement.innerText = currentQuantity;
+    }
+
+    function toggleShoppingCart() {
+      // Target the nested UL by its ID
+      var menuBurgerIcon = document.getElementById('menu-burger-icon');
+      menuBurgerIcon.style.zIndex = '0';
+  
+      var shoppingCart = document.getElementById('shopping-cart');
+      
+      // Toggle the display property
+      if (shoppingCart.style.display === 'none' || shoppingCart.style.display === '') {
+          shoppingCart.style.display = 'block';
+      } else {
+          shoppingCart.style.display = 'none';
+          menuBurgerIcon.style.zIndex = '1100';
+      }
+  }
